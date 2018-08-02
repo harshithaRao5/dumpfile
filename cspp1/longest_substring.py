cf531bebccfd5e4dd903994bb26e0ab8401e67f4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-02 20:22:00 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1/longest_substring.py
+++ cspp1/longest_substring.py
@@ -3,15 +3,15 @@ S = input()
 i = S[0]
 j = ''
 "'#checking for the string present in range'"
-for a in range(1,len(S)):
+for a in range(1, len(S)):
     if S[a] >= i[len(i)-1]:
         i += S[a]
     else:
         if len(i) > len(j):
             j = i
+        i = S[a]
+"'#checking that which string is greater'"
+if len(i) > len(j):
+    j = i
     
-        i=S[a]
-        
-if len(i)> len(j):
-    j=i
-print (j)
+print(j)
--- cspp1/p3/p3/longest_substring.py
+++ cspp1/p3/p3/longest_substring.py
@@ -1,15 +1,15 @@
+"'#finding the longest substring'"
 S = input()
 i = S[0]
 j = ''
-for a in range(1,len(S)):
+"'#checking for the string present in range'"
+for a in range(1, len(S)):
     if S[a] >= i[len(i)-1]:
         i += S[a]
     else:
         if len(i) > len(j):
             j = i
-    
-        i=S[a]
-        
-if len(i)> len(j):
-    j=i
-print (j)
+        i = S[a]
+if len(i) > len(j):
+    j = i
+print(j)
