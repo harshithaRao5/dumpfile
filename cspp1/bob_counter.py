cadff2c23c0c4544a11028f34a2f8ecc2a5ad823
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-02 17:29:06 +0530

    " -> 0 of 1 passed. pylint: 6.00/10 "

---
+++ cspp1/bob_counter.py
@@ -0,0 +1,14 @@
+"'#number of bob present in the given string'"
+S = input("enter the string")
+COUNT = 0
+LENGTH = int(len(S))
+A = 0
+B = 3
+"'#checking that the limit doen't extend the length of the given string'"
+while B <= LENGTH:
+    Z = S[A:B]
+    if Z == 'bob':
+        COUNT = COUNT + 1
+    A = A + 1
+    B = B + 1
+print("number of times bob occured:", COUNT)
--- cspp1/sum_1_end.py
+++ cspp1/sum_1_end.py
@@ -1,4 +1,4 @@
-sum=0
+sum=int(input())
 for i in range(1,7,1):
     sum=sum+i
 print(sum)
