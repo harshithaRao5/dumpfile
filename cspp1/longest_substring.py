5cdee511a30ceb7198d2de92feb1b115ea259dae
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-02 20:04:24 +0530

    " -> 5 of 5 passed. pylint: 3.33/10 "

--- cspp1/longest_substring.py
+++ cspp1/longest_substring.py
@@ -1,6 +1,8 @@
+"'#finding the longest substring'"
 S = input()
 i = S[0]
 j = ''
+"'#checking for the string present in range'"
 for a in range(1,len(S)):
     if S[a] >= i[len(i)-1]:
         i += S[a]
