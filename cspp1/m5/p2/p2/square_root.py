bb3b0c7089f12c2d547589dbae8a82c4777f106a
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-03 18:28:07 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1/m5/p2/p2/square_root.py
+++ cspp1/m5/p2/p2/square_root.py
@@ -1,9 +1,11 @@
-# Write a python program to find the square root of the given number 
-# using approximation method
-
-def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+"'#finding the square root of the given number'"
+SQUARE = int(input())
+i = 0.1
+GUESS = 0
+j = 0.1
+while abs(GUESS**2 - SQUARE) >= i and GUESS <= SQUARE:
+    GUESS += j
+if abs(GUESS**2 - SQUARE) >= i:
+    print('Failed on square root of', SQUARE)
+else:
+    print(str(GUESS))
--- cspp1/m5/p3/p3/square_root_bisection.py
+++ cspp1/m5/p3/p3/square_root_bisection.py
@@ -1,15 +1,13 @@
 "'#finding the square root of number using bisection method'"
 X = int(input())
 i = 0.01
-Low = 0
-High = X
-Ans = (High + Low)/2.0
-while abs(Ans**2 - X) >= i:
-    if Ans**2 < X:
-        Low = Ans
+LOW = 0
+HIGH = X
+ANS = (HIGH + LOW)/2.0
+while abs(ANS**2 - X) >= i:
+    if ANS**2 < X:
+        LOW = ANS
     else:
-        High = Ans
-    Ans = (High + Low)/2.0
-print(Ans)
-
-
+        HIGH = ANS
+    ANS = (HIGH + LOW)/2.0
+print(ANS)
