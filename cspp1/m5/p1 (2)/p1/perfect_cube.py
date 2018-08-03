bf04149fb5f0aca8b88eedc0057fa2c4f19d7f66
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-03 17:22:43 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1/m5/p1 (2)/p1/perfect_cube.py
+++ cspp1/m5/p1 (2)/p1/perfect_cube.py
@@ -5,7 +5,7 @@ for GUESS in range(abs(CUBE)+1):
     if GUESS**3 >= abs(CUBE):
         break
 if GUESS**3 != abs(CUBE):
-    print(str(CUBE)+ "is not a perfect cube")
+    print(str(CUBE)+ " is not a perfect cube")
 else:
     if CUBE < 0:
         GUESS = -GUESS
