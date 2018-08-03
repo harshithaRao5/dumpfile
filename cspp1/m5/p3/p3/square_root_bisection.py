4aa68c2c021ad4a538f83eac1a3b7e34f5733454
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-03 17:50:37 +0530

    " -> 3 of 3 passed. pylint: 3.64/10 "

--- cspp1/m5/p3/p3/square_root_bisection.py
+++ cspp1/m5/p3/p3/square_root_bisection.py
@@ -1,6 +1,6 @@
 "'#finding the square root of number using bisection method'"
 X = int(input())
-i = 0.1
+i = 0.01
 Low = 0
 High = X
 Ans = (High + Low)/2.0
@@ -10,6 +10,6 @@ while abs(Ans**2 - X) >= i:
     else:
         High = Ans
     Ans = (High + Low)/2.0
-print(str(Ans))
+print(Ans)
 
 
