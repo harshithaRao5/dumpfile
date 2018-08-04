dcf5dc5deb80f5b0dc2c76ff78f742d53cd14ffa
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-04 12:03:45 +0530

    " -> 7 of 7 passed. pylint: 0.83/10 "

--- cspp1/m6/digit_product.py
+++ cspp1/m6/digit_product.py
@@ -1,10 +1,10 @@
-n=int(input())
-r=0
-c=1
-i=0
-while i<=n:
-    r=n%10
-    c=c*r
-    n=n//10
-    i=i+1
-print(str(c))
+N = int(input())
+R = 0
+C = 1
+i = 0
+while i <= N:
+    R = N % 10
+    C = C * R
+    N = N // 10
+    i = i + 1
+print(str(C))
--- cspp1/m6/special_char.py
+++ cspp1/m6/special_char.py
@@ -1,6 +1,9 @@
 s=input()
-for z in s:
+l=len(s)
+z='!@#$%^&*'
+for z in range(l):
     if z=='!@#$%^&*':
-        z=1
-        
+        print(z="0")
 print(s)
+
+   
