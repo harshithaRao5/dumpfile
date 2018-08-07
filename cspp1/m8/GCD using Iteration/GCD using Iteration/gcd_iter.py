e06636f57abc4d347bb4154bb59f7c1a1b3530d4
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-07 16:23:32 +0530

    " -> 9 of 10 passed. pylint: -3.33/10 "

---
+++ cspp1/m8/GCD using Iteration/GCD using Iteration/gcd_iter.py
@@ -0,0 +1,20 @@
+# Exercise: GCDIter
+# Write a Python function, gcdIter(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
+
+# This function takes in two numbers and returns one number.
+
+
+def gcdIter(a, b):
+    i=1
+    while i <= a and i <= b:
+    	if(a%i==0 and b%i==0):
+    		gcd = i
+    	i += 1
+    return gcd
+def main():
+    data = input()
+    data = data.split()
+    print(gcdIter(int(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
---
+++ cspp1/m8/power using Iteration/power using Iteration/power_iter.py
@@ -0,0 +1,23 @@
+# Exercise: PowerIter
+# Write a Python function, iterPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
+
+# This function takes in two numbers and returns one number.
+
+
+def iterPower(base, exp):
+    res=1
+    i=1
+    while i <= exp:
+        res = res * base
+        i = i + 1
+    return res
+
+
+def main():
+
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
