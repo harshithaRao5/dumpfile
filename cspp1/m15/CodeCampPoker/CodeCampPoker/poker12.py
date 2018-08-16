71b8ed858477b5cca099be2dc14447746b967793
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-16 12:31:01 +0530

    " -> 16 of 19 passed. pylint: 7.86/10 "

--- cspp1/m15/CodeCampPoker/CodeCampPoker/poker12.py
+++ cspp1/m15/CodeCampPoker/CodeCampPoker/poker12.py
@@ -82,13 +82,29 @@ def two_pair(hand):
             return True
     return False
 def high_card(hand):
-    face_value3 = []
+    '''face_value3 = []
     for H in hand:
         face_value3.append(card_values[H[0]])
     for i in range(0, len(face_value3)-1):
         if face_value3[i] == 14 or face_value3[i] == 13 or face_value3[i] == 12 or face_value3[i] == 11 or face_value3[i] == 10:
             return True
-    return False
+    return False'''
+    num_ = []
+    for i in hand:
+        if i[0] == 'T':
+            num_.append(10)
+        elif i[0] == 'J':
+            num_.append(11)
+        elif i[0] == 'Q':
+            num_.append(12)
+        elif i[0] == 'K':
+            num_.append(13)
+        elif i[0] == 'A':
+            num_.append(14)
+        else:
+            num_.append(int(i[0]))
+    return max(num_)
+    
     
 def hand_rank(hand):
     '''
