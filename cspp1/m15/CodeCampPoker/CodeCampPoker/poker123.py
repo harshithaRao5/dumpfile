49317347357195e612401ad8560b65212577bd89
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-16 18:55:04 +0530

    " -> 19 of 19 passed. pylint: 9.67/10 "

--- cspp1/m15/CodeCampPoker/CodeCampPoker/poker123.py
+++ cspp1/m15/CodeCampPoker/CodeCampPoker/poker123.py
@@ -3,7 +3,7 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-card_values = {'T':10,'J':11,'Q':12,'K':13,'A':14,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+CARD_VALUES = {'T':10, 'J':11, 'Q':12, 'K':13, 'A':14, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -14,12 +14,13 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    face_values=[]
-    for h in hand:
-        face_values.append(card_values[h[0]])
+    face_values = []
+    for h_1 in hand:
+        face_values.append(CARD_VALUES[h_1[0]])
     face_values.sort()
+    i = 0
     for i in range(0, len(face_values)-1):
-        if face_values[i+1]-face_values[i]!=1:
+        if face_values[i+1]-face_values[i] != 1:
             return False
     return True
 
@@ -33,26 +34,28 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
-    suit=hand[0]
-    for h in hand:
-        if suit[1]!=h[1]:
+    suit = hand[0]
+    for h_1 in hand:
+        if suit[1] != h_1[1]:
             return False
     return True
 def card_ranks(hand):
-
-    ranks=sorted(['--23456789TJQKA'.index(c) for c,s in hand])
+    '''returns the max rank'''
+    ranks = sorted(['--23456789TJQKA'.index(c) for c, s in hand])
     ranks.reverse()
     return ranks
-def kind(ranks, n):
-    for r in ranks:
-        if ranks.count(r) == n:
-            return r
+def kind(ranks, n_1):
+    '''returns whether one or three or four are of same kind'''
+    for r_1 in ranks:
+        if ranks.count(r_1) == n_1:
+            return r_1
     return 0
 def two_pair(ranks):
-    one=kind(ranks,2)
-    two=kind(sorted(ranks),2)
-    if one and two:
-        return (one,two)
+    '''checks two pairs are equal or not'''
+    one_1 = kind(ranks, 2)
+    two_2 = kind(sorted(ranks), 2)
+    if one_1 and two_2:
+        return (one_1, two_2)
     return None
 def hand_rank(hand):
     '''
@@ -62,29 +65,24 @@ def hand_rank(hand):
         The first version should identify if the given hand is a straight
         or a flush or a straight flush.
     '''
-    ranks=card_ranks(hand)
+    ranks = card_ranks(hand)
     if is_straight(hand) and is_flush(hand):
-        return (8,ranks)
-    if kind(ranks,4):
-        return (7,kind(ranks,4),ranks)
-    if kind(ranks,3) and kind(ranks,2):
-        return (6,(kind(ranks,3),kind(ranks,2)))
+        return(8, ranks)
+    if kind(ranks, 4):
+        return(7, kind(ranks, 4), ranks)
+    if kind(ranks, 3) and kind(ranks, 2):
+        return(6, (kind(ranks, 3), kind(ranks, 2)))
     if is_flush(hand):
-        return (5,ranks)
+        return(5, ranks)
     if is_straight(hand):
-        return (4,ranks)
-    if kind(ranks,3):
-        return(3,kind(ranks,3),ranks)
+        return(4, ranks)
+    if kind(ranks, 3):
+        return(3, kind(ranks, 3), ranks)
     if two_pair(ranks):
-        return(2,two_pair(ranks),ranks)
-    if kind(ranks,2):
-        return(1,kind(ranks,2),ranks)
-    return(0,ranks)
-
-
-
-    
-
+        return(2, two_pair(ranks), ranks)
+    if kind(ranks, 2):
+        return(1, kind(ranks, 2), ranks)
+    return(0, ranks)
 def poker(hands):
     '''
         This function is completed for you. Read it to learn the code.
