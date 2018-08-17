cd77fecaa62c19feec6bf9a19e7411f207e5c565
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-17 13:54:11 +0530

    " -> 1 of 3 passed. pylint: 9.15/10 "

--- cspp1/m16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ cspp1/m16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -11,12 +11,12 @@ def similarity(dict1, dict2):
     word_list2 = ''
     for i in dict1:
         for j in i:
-            if j not in ',.=!@#$%^&?*()_+-1234567890':
+           if j not in '!@#$%^&*()_+-=,.?1234567890':
                 if j not in "'":
                     word_list1 = word_list1 + j
     for i in dict2:
         for j in i:
-            if j not in ',.=!@#$%^&*?()_+-1234567890':
+            if j not in '!@#$%^&*()_+-=,.?1234567890':
                 if j not in "'":
                     word_list2 = word_list2 + j
     word_list1 = dict1.split()
@@ -25,7 +25,7 @@ def similarity(dict1, dict2):
     dict3={}
     for word in word_list3:
         if word not in load_stopwords(file_name).keys():
-            dict3[word] = (word_list1.count(word),word_list2.count(word))
+            dict3[word] = (word_list1.count(word), word_list2.count(word))
     numerator = 0
     denominator = 0
     sum_1 = 0
@@ -35,7 +35,7 @@ def similarity(dict1, dict2):
         sum_1 = sum_1 + dict3[i][0]**2
         sum_2 = sum_2 + dict3[i][1]**2
     denominator = math.sqrt(sum_1) * math.sqrt(sum_2)
-    xyz = (numerator/denominator)
+    xyz = (numerator / denominator)
     return xyz
 
 def load_stopwords(filename):
