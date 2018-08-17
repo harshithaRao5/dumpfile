230b66fcb04724d41ee0142c3664abef4e8ba8ed
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-17 12:41:12 +0530

    " -> 1 of 3 passed. pylint: 7.71/10 "

--- cspp1/m16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
+++ cspp1/m16/CodeCampDocumentDistance/CodeCampDocumentDistance/document_distance.py
@@ -23,9 +23,9 @@ def similarity(dict1, dict2):
     sum_2 = 0
     for i in dict3:
         numerator = numerator + (dict3[i][0]*dict3[i][1])
-        sum_1 = dict3[i][0]**2
-        sum_2 = dict3[i][1]**2
-    denominator = denominator + (math.sqrt(sum_1))*(math.sqrt(sum_2))
+        sum_1 += dict3[i][0]**2
+        sum_2 += dict3[i][1]**2
+    denominator = (math.sqrt(sum_1))*(math.sqrt(sum_2))
     return (numerator/denominator)
 
 def load_stopwords(filename):
