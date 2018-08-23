4f3979162c13541810ff03cb79a8e7b2a20f92c2
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-23 13:31:05 +0530

    " -> 0 of 8 passed. pylint: 5.56/10 "

--- cspp1/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -6,12 +6,16 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    result = [[0,0,0],[0,0,0],[0,0,0]]
-    for i in range(len(m1)):
-       for j in range(len(m1[0])):
-           for k in range(len(m2)):
-               result[i][j] += int(m1[i][k]) * int(m2[k][j])
-    return result
+    mul_matrix = [[0 for row in range(len(m1))] for column in range(len(m1[0]))]
+    if len(m1[0]) == len(m2):
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                for k in range(len(m2[0])):
+                    mul_matrix[i][j] += int(m1[i][k]) * int(m2[k][j])
+    else:
+        print("Error: Matrix shapes invalid for mult")
+    return mul_matrix
+    
 
 def add_matrix(m1, m2):
     '''
@@ -21,12 +25,14 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    result = [[0,0,0],[0,0,0],[0,0,0]]
-    for i in range(len(m1)):
-       for j in range(len(m1[0])):
-           result[i][j] = int(m1[i][j]) + int(m2[i][j])
-    return result
-    
+    if len(m1[0]) == len(m2):
+        sum_matrix = m1
+        for i in range(len(m1)):
+            for j in range(len(m1[0])):
+                sum_matrix[i][j] = int(m1[i][j]) + int(m2[i][j])
+    else:
+        print("Error: Matrix shapes invalid for addition")
+    return sum_matrix
 
 def read_matrix(m1, m2):
     '''
@@ -36,9 +42,8 @@ def read_matrix(m1, m2):
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    if m1[0] == m2[0] and m1[1] == m2[1]:
-        return True
-    return False
+
+    
 
 
 def main():
@@ -49,7 +54,8 @@ def main():
     n_1 = int(rows_columns[0])
     matrix_1 = []
     for _ in range(n_1):
-        matrix_1.append(list(map(int, input().rstrip().split()))) 
+        matrix_1.append(list(map(int, input().rstrip().split())))
+    #print(matrix_1)
     # read matrix 2
     rows_columns1 = []
     for i in range(1):
@@ -58,9 +64,10 @@ def main():
     matrix_2 = []
     for _ in range(n_2):
         matrix_2.append(list(map(int, input().rstrip().split())))
+    #print(matrix_2)
 
     # add matrix 1 and matrix 2
-    print(add_matrix(matrix_1, matrix_2))
+    #print(add_matrix(matrix_1, matrix_2))
 
     # multiply matrix 1 and matrix 2
     print(mult_matrix(matrix_1, matrix_2))
