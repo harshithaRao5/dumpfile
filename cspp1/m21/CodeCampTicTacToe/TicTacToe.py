df4e9100f491faeb013cb53dcf277f5d6ee8197c
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-24 10:48:22 +0530

    " -> 6 of 14 passed. pylint: 7.00/10 "

--- cspp1/m21/CodeCampTicTacToe/TicTacToe.py
+++ cspp1/m21/CodeCampTicTacToe/TicTacToe.py
@@ -1,32 +1,30 @@
 '''program for checking various conditions in game'''
 def Tic_Tac_Toe(matrix):
     '''function for playing TicTacToe game'''
-    winner = []
+    result = []
     for i in range(0, 3):
         if matrix[i][0] == matrix[i][1] == matrix[i][2]:
-            winner.append(matrix[i][0])
+            result.append(matrix[i][0])
     '''for row in matrix:
         if row[0] == row[1] == row[2]:
-           winner.append(row[0])'''
+           result.append(row[0])'''
     for i in range(0, 3):
         if matrix[0][i] == matrix[1][i] == matrix[2][i]:
-           winner.append(matrix[0][i])
+           result.append(matrix[0][i])
     if matrix[0][0] == matrix[1][1] == matrix[2][2]:
-           winner.append(matrix[0][0])
+           result.append(matrix[0][0])
     if matrix[2][0] == matrix[1][1] == matrix[0][2]:
-        winner.append(matrix[0][2])
-    if len(winner) == 0:
+        result.append(matrix[0][2])
+    if len(result) == 0:
         print('draw')
         return None
-    if len(winner) == 1:
-        if winner[0] == 'x' or winner[0] == 'o':
-           print(winner[0])
-        else:
-           print("invalid input")
-        return winner[0]
-    else:
-        print("invalid game")
-        return None
+    if len(result) == 1:
+        if result[0] == 'x' or result[0] == 'o':
+           print(result[0])
+        print("invalid input")
+        return result[0]
+    print("invalid game")
+    return None
        
 def main():
     '''main function '''
