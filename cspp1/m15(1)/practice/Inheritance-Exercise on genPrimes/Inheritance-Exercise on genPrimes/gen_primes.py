cc5224fd67d136d978ed6de90e1696041c593c5f
harshithaRao5 <harshithanallani@msitprogram.net>
2018-08-24 19:53:06 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1/m15(1)/practice/Inheritance-Exercise on genPrimes/Inheritance-Exercise on genPrimes/gen_primes.py
+++ cspp1/m15(1)/practice/Inheritance-Exercise on genPrimes/Inheritance-Exercise on genPrimes/gen_primes.py
@@ -1,17 +1,20 @@
 #define the gen_primes function here
 def genPrimes():
-	
-    pass
+	while True:
+        if is_prime(number):
+            yield number
+        	print(number)
+    
 
 def main():
-	data=raw_input()
+	datainput()
 	l=data.split()
 	a=int(l[0])
 	b=int(l[1])
 	primeGenerator = genPrimes()
 	for i in range(a):
 	    p=primeGenerator.next()
-	    if(i%b==0):
+	    if i%b == 0:
 	        print p
 	
 if __name__== "__main__":
